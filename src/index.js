fa2b58f26eff59080b2b2eaa271189f8ce9e5788
wazirwazir <abidakunwazir@gmail.com>
2024-09-27 16:08:52 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-
-
+// app.js
+let dett;
 document.addEventListener('DOMContentLoaded', () => {
     fetch('src/data.json')
         .then(response => {
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
             return response.json();
         })
         .then(data => {
-            main.innerHTML = renderJobs(data)
+            const output = document.getElementById('output');
+            dett = data
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error);
@@ -18,10 +19,166 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+
+
+
+
+const filteredSpace = document.querySelector('.filtered')
 const main = document.querySelector('main')
 
+let clearBtn = document.querySelector('.clearBtn')
 let searchArray = []
-
+let sample = [
+    {
+        "id": 1,
+        "company": "Photosnap",
+        "logo": "./images/photosnap.svg",
+        "new": true,
+        "featured": true,
+        "position": "Senior Frontend Developer",
+        "role": "Frontend",
+        "level": "Senior",
+        "postedAt": "1d ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": ["HTML", "CSS", "JavaScript"],
+        "tools": []
+    },
+    {
+        "id": 2,
+        "company": "Manage",
+        "logo": "./images/manage.svg",
+        "new": true,
+        "featured": true,
+        "position": "Fullstack Developer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "1d ago",
+        "contract": "Part Time",
+        "location": "Remote",
+        "languages": ["Python"],
+        "tools": ["React"]
+    },
+    {
+        "id": 3,
+        "company": "Account",
+        "logo": "./images/account.svg",
+        "new": true,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2d ago",
+        "contract": "Part Time",
+        "location": "USA Only",
+        "languages": ["JavaScript"],
+        "tools": ["React", "Sass"]
+    },
+    {
+        "id": 4,
+        "company": "MyHome",
+        "logo": "./images/myhome.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "5d ago",
+        "contract": "Contract",
+        "location": "USA Only",
+        "languages": ["CSS", "JavaScript"],
+        "tools": []
+    },
+    {
+        "id": 5,
+        "company": "Loop Studios",
+        "logo": "./images/loop-studios.svg",
+        "new": false,
+        "featured": false,
+        "position": "Software Engineer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "1w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript", "Ruby"],
+        "tools": ["Sass"]
+    },
+    {
+        "id": 6,
+        "company": "FaceIt",
+        "logo": "./images/faceit.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Backend Developer",
+        "role": "Backend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "UK Only",
+        "languages": ["Ruby"],
+        "tools": ["RoR"]
+    },
+    {
+        "id": 7,
+        "company": "Shortly",
+        "logo": "./images/shortly.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["HTML", "JavaScript"],
+        "tools": ["Sass"]
+    },
+    {
+        "id": 8,
+        "company": "Insure",
+        "logo": "./images/insure.svg",
+        "new": false,
+        "featured": false,
+        "position": "Junior Frontend Developer",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "2w ago",
+        "contract": "Full Time",
+        "location": "USA Only",
+        "languages": ["JavaScript"],
+        "tools": ["Vue", "Sass"]
+    },
+    {
+        "id": 9,
+        "company": "Eyecam Co.",
+        "logo": "./images/eyecam-co.svg",
+        "new": false,
+        "featured": false,
+        "position": "Full Stack Engineer",
+        "role": "Fullstack",
+        "level": "Midweight",
+        "postedAt": "3w ago",
+        "contract": "Full Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript", "Python"],
+        "tools": ["Django"]
+    },
+    {
+        "id": 10,
+        "company": "The Air Filter Company",
+        "logo": "./images/the-air-filter-company.svg",
+        "new": false,
+        "featured": false,
+        "position": "Front-end Dev",
+        "role": "Frontend",
+        "level": "Junior",
+        "postedAt": "1mo ago",
+        "contract": "Part Time",
+        "location": "Worldwide",
+        "languages": ["JavaScript"],
+        "tools": ["React", "Sass"]
+    }]
     let filteredJobs;
     const filterJobs = (jobs, terms) => {
         return jobs.filter(job => 
@@ -67,7 +224,9 @@ function renderJobs(e) {
             </div>
             `}).join('')
     }
-    
+    let getter = renderJobs(sample)
+    let gettest =      
+    main.innerHTML = getter
 
     // Renders keywords
     function InsertKeyword(e) {
@@ -91,19 +250,29 @@ function renderJobs(e) {
             const text = removeBtn.parentElement.querySelector('p').textContent
             searchArray = searchArray.filter(me => me !== text)
             removeBtn.parentElement.remove()
+            filteredSpace.replaceWith(main)
         })
-        // Update filtergggguik
+
         filteredJobs = filterJobs(sample, searchArray)
     }
     
-  // Inserts keyword in search
-    const keys = document.querySelectorAll('.keys')
+    // Inserts keyword in search
+    let keys = document.querySelectorAll('.keys')
     keys.forEach(key => {
         key.addEventListener('click', () => {
             if  (!searchArray.includes(key.textContent)){
                 InsertKeyword(key)
+                
+                filteredSpace.innerHTML = renderJobs(filteredJobs)
+                keys = document.querySelectorAll('.keys')
+                main.replaceWith(filteredSpace)
                 console.log('pick')
-               //main.innerHTML = renderJobs(filteredJobs)
             }
         })
     });
+
+    clearBtn.addEventListener('click', () => {
+        const searchBox = document.querySelector('search')
+        const searchs = searchBox.querySelectorAll('.searchs')
+        remove(searchs)
+    })
